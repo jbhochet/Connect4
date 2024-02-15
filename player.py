62c2c62d9ad7fe5d09044b56c70a10b9580ab5fe
Jb Hochet <jean-baptiste.hochet@proton.me>
2024-02-15 10:45:33 +0100

    Implement HumanPlayer class

--- player.py
+++ player.py
@@ -1,11 +1,21 @@
 from board import Board
 
+
 class Player:
     def play(self, symbol: str, board: Board) -> int:
         """
         Return the column number to play the player move.
         """
 
+
 class HumanPlayer(Player):
     def play(self, symbol: str, board: Board) -> int:
-        pass
+        colx = -1
+        while colx < 0:
+            try:
+                colx = int(
+                    input(f"Player {symbol}, which column do you want to play in? ")
+                )
+            except:
+                colx = -1
+        return colx
